b35289227518489ebb5af783d3e5551e252b5ddb
Pavel Markitantov <markitantov@mail.com>
2024-04-13 22:32:41 +0200

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Pavel Markitantov on 13/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
