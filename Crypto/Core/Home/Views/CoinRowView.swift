f91f1c82e2bed29dc2d2248a2faf46c1c815e312
Pavel Markitantov <markitantov@mail.com>
2024-04-22 17:45:41 +0200

    feat: Add CoinRowView for displaying coins in List, include mock data for previews, and add extension for Double to format values

---
+++ Crypto/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,70 @@
+//
+//  CoinRowView.swift
+//  Crypto
+//
+//  Created by Pavel Markitantov on 22/04/2024.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    let coin: CoinModel
+    let showHoldingsColumn: Bool
+
+    var body: some View {
+        HStack(spacing: 0) {
+            leftColumn
+            Spacer()
+            if showHoldingsColumn {
+                midColumn
+            }
+            rightColumn
+        }
+        .font(.subheadline)
+    }
+}
+
+#Preview {
+    CoinRowView(coin: MockData.coin, showHoldingsColumn: true)
+        
+}
+
+extension CoinRowView {
+    private var leftColumn: some View {
+        HStack(spacing: 0) {
+            Text("\(coin.rank)")
+                .font(.caption)
+                .foregroundStyle(Color.theme.secondaryText)
+                .frame(minWidth: 30)
+            Circle()
+                .frame(width: 30, height: 30)
+            Text(coin.symbol.uppercased())
+                .font(.headline)
+                .padding(.leading, 6)
+                .foregroundStyle(Color.theme.accent)
+        }
+    }
+
+    private var midColumn: some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentHoldingsValue.asCurrencyWith6Decimals())
+                .bold()
+            Text((coin.currentHoldings ?? 0).asNumberString())
+        }
+        .foregroundStyle(Color.theme.accent)
+    }
+
+    private var rightColumn: some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentPrice.asCurrencyWith6Decimals())
+                .bold()
+                .foregroundStyle(Color.theme.accent)
+            Text(coin.priceChangePercentage24H?.asPercentString() ?? "")
+                .foregroundStyle((coin.priceChangePercentage24H ?? 0) >= 0 ?
+                    Color.theme.green :
+                    Color.theme.red
+                )
+        }
+        .frame(width: UIScreen.main.bounds.width / 3)
+    }
+}
